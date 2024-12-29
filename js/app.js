80519fd8099109b03cc4ac2ad2b6bb9c4fad3ef9
borystimo <141815755+borystimo@users.noreply.github.com>
2024-12-29 19:44:35 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,72 @@
+"use strict"
+
+import * as flsFunctions from "./modules/functions.js";
+flsFunctions.isWebp();
+
+
+//? Импорт swiper----------------------
+// import * as mySwiper from "./swiper.js"
+
+
+//? Импорт header burger----------------------
+import * as myheader from "./header.js"
+// import isMobile from "./header.js"
+
+
+//? Импорт rating----------------------
+// import * as myRating from "./rating.js"
+
+//?dynamicAdapt------------------
+import { useDynamicAdapt } from './dynamicAdapt.js'
+useDynamicAdapt()
+
+
+//?Popup----------------------
+import * as myPopup from "./popup.js"
+
+//? Диалоговое окно----------------------
+// import * as myNonModal from "./non-modal.js"
+
+
+//? Menucase----------------------
+// import * as myMenuCase from "./menucase.js"
+
+//? Light / Dark mode----------------------
+// import * as lightDarkMode  from "./light-dark-mod.js"
+
+//? Accordion / Spollers----------------------
+// import * as AccordionSpollers  from "./accordion-spollers.js"
+
+//? 3d Cards----------------------
+// import * as ThreeDcards  from "./3dcard.js"
+
+//? active-header-link----------------------
+// import * as activeHeaderLink  from "./active-header-link.js"
+
+//? tabs----------------------
+// import * as tabsContent  from "./tabs.js"
+
+//? scrollToTop----------------------
+import * as scrollToTop  from "./scroll-to-top.js"
+
+//? rightside panel----------------------
+// import * as asideMenu  from "./rightside-menu.js"
+
+//? breadcrumbs----------------------
+// import * as breadCrumbs  from "./breadcrumbs.js"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
